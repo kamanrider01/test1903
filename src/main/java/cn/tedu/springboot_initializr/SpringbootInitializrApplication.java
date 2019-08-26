4e40b56749a055540ae709e2e1714b3f1f402e26
44174 <441749611@qq.com>
2019-08-26 19:47:19 +0800

    初次导入

---
+++ src/main/java/cn/tedu/springboot_initializr/SpringbootInitializrApplication.java
@@ -0,0 +1,14 @@
+package cn.tedu.springboot_initializr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class SpringbootInitializrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootInitializrApplication.class, args);
+    }
+
+}
